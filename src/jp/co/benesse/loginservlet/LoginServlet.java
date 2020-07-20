deba53c892e5f3dc5f9008430ad2ee94275b0a3f
shum-rmq <shunpei.kuri.fgt22@gmail.com>
2020-07-20 18:38:19 +0900

    LoginServletの大枠作成

--- src/jp/co/benesse/loginservlet/LoginServlet.java
+++ src/jp/co/benesse/loginservlet/LoginServlet.java
@@ -1,7 +1,5 @@
 package jp.co.benesse.loginservlet;
-//test
 
-//test2
 import java.io.IOException;
 import java.sql.Connection;
 
@@ -14,9 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import jp.co.benesse.dataaccess.cm.ConnectionManager;
+import jp.co.benesse.dataaccess.crypt.CryptographyLogic;
 import jp.co.benesse.dataaccess.dao.UserDAO;
 import jp.co.benesse.dataaccess.value.UserBean;
-
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -33,46 +31,35 @@ public class LoginServlet extends HttpServlet {
 
 		String mail = request.getParameter("mail");
 		String password = request.getParameter("password");
-
-
-		//正しいユーザーかの判定
-
-		//DBから入力されたuserIDの情報を取得
-
+		String hash = CryptographyLogic.encryptStr(password);
 		ConnectionManager connectionManager = new ConnectionManager();
 		UserBean userBean = new UserBean();
+
 
 		try {
 			Connection connection = connectionManager.getConnection();
 			UserDAO userDAO = new UserDAO(connection);
-			User user = userDAO.selectByUserId(Integer.parseInt(userId));
+			userBean = userDAO.findUser(mail, hash);
 
-			if(user == null){
-				request.setAttribute("inputIDerrorMsg", "入力したユーザーIDは未登録です。");
-				RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/userLogin.jsp");
+
+			if(userBean == null){
+				request.setAttribute("popFlag", 2);
+				RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/login/login.jsp");
 				dispatcher.forward(request, response);
 				return;
 			}
 
-			if(!(user.getPassword().equals(password))){
-				request.setAttribute("inputPassworderrorMsg", "パスワードが間違っています。");
-				RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/userLogin.jsp");
-				dispatcher.forward(request, response);
-				return;
-			}
-
-			//セッションにインスタンスの設定
 			HttpSession session = request.getSession(true);
-			session.setAttribute("user", user);
+			session.setAttribute("userId", userBean.getUserId());
+			session.setAttribute("userName", userBean.getUserName());
+
+			response.sendRedirect("/user/calendar");
+			return;
+
 
 		} finally {
 			connectionManager.closeConnection();
-
 		}
-
-		//リダイレクト
-		response.sendRedirect("menu");
-		return;
 
 	}
 
