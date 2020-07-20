d678acfe086f4dfafc021500e22c775872b48406
mmatsui-6454 <452012@tokyo.soka.ed.jp>
2020-07-20 10:51:59 +0900

    Servlet・JSP接続確認

--- WebContent/WEB-INF/views/login/login.jsp
+++ WebContent/WEB-INF/views/login/login.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+ログイン画面
 </body>
 </html>
--- src/jp/co/benesse/loginservlet/LoginServlet.java
+++ src/jp/co/benesse/loginservlet/LoginServlet.java
@@ -4,6 +4,7 @@ package jp.co.benesse.loginservlet;
 //test2
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,32 +14,17 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/login/login.jsp");
+		dispatcher.forward(request, response);
+		return;
 	}
 
 }
