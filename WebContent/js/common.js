be8ee322f674e1cfd234a301414f5400822026f2
nakane-benesse <nakane.benesse@gmail.com>
2020-07-20 10:21:51 +0900

    テンプレ作成しました！

---
+++ WebContent/js/common.js
@@ -0,0 +1,28 @@
+
+//共通パーツ
+//基本的にここをさわらない
+
+// JavaScript Document
+$(function () {
+  $('.small-popup-button').click(function () {
+    $('.error-popup').fadeIn();
+  });
+
+  $('.large-popup-button').click(function () {
+    $('.confirm-popup').fadeIn();
+  });
+
+  $('.back-popup-button').click(function () {
+    $('.back-popup').fadeIn();
+  });
+
+
+  // 閉じるボタン
+  $('.close-popup').click(function () {
+    $('.confirm-popup').fadeOut();
+    $('.error-popup').fadeOut();
+    $('.back-popup').fadeOut();
+  })
+
+
+});
