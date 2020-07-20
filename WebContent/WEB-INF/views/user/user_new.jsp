55ed8337134ad7da22a641ebeb8945cbc6999f0e
yamamocchan <miyukiyamamoto564@gmail.com>
2020-07-20 14:59:12 +0900

    仮コミット

---
+++ WebContent/WEB-INF/views/user/user_new.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link href="css/app.css" rel="stylesheet" type="text/css">
+<link href="css/common.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<title>アカウント作成</title>
+</head>
+<body>
+
+<%@ include file="common/header.jsp" %>
+<article>
+
+
+
+
+
+
+
+
+</article>
+<%@ include file="common/footer.jsp" %>
+<script src="js/common.js"></script>
+</body>
+</html>
---
+++ src/jp/co/benesse/userservlet/UserCreateServlet.java
@@ -0,0 +1,80 @@
+package jp.co.benesse.userservlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jp.co.benesse.dataaccess.cm.ConnectionManager;
+import jp.co.benesse.dataaccess.dao.UserDAO;
+import jp.co.benesse.dataaccess.value.UserBean;
+
+/**
+ * Servlet implementation class UserNewServlet
+ */
+@WebServlet("/user/usercreate")
+public class UserCreateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//doGetされたものをdoPostに変換
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userName = request.getParameter("userName");
+		String mail = request.getParameter("mail");
+		String password = request.getParameter("password");
+		ConnectionManager connectionManager = new ConnectionManager();
+		try{
+			Connection connection = connectionManager.getConnection();
+			// UserDAOの作成
+			UserDAO userDAO = new UserDAO(connection);
+
+			//メールアドレスかぶりの確認
+			if(userDAO.isBooking(mail)){
+				//かぶっていたらメールアドレス重複のポップアップが出るようにフラグ立て。
+				request.setAttribute("popFlag",1);
+				//アカウント新規登録画面へ戻る。
+				RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/view/user/user_new.jsp");
+				dispatcher.forward(request, response);
+			}
+
+			//新規登録をするために
+			int result = userDAO.createUser(userName,mail,password);
+			if(result!=1){
+				RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/view/error/error.jsp");
+				dispatcher.forward(request, response);
+			}
+		}
+		catch(RuntimeException e){
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/view/error/error.jsp");
+			dispatcher.forward(request, response);
+			return;
+		}
+		finally{
+			connectionManager.closeConnection();
+		}
+		UserBean userBean = new UserBean();
+		userBean = userDAO.findUser(mail,password);
+		HttpSession session = request.getSession();
+		session.setAttribute("userName",userBean.getUserName());
+		session.setAttribute("userId",userBean.getUserId());
+		//新規登録完了ポップアップを出すためのフラグを立てる。
+		request.setAttribute("popFlag",0);
+		//user_new.jsp(アカウント新規作成画面)にforwardする。
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/view/user/user_new.jsp");
+		dispatcher.forward(request, response);
+		return;
+	}
+
+}
