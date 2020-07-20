5aec454e2feb37a6f04a3239fd9dcaa92a27d6c7
limeJP <a_b_l_c@icloud.com>
2020-07-20 10:31:54 +0900

    TestNagasawa

--- src/jp/co/benesse/userservlet/Test.java
+++ src/jp/co/benesse/userservlet/Test.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Test
  */
-@WebServlet("/Test")
+@WebServlet("/TestNagasawa")
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
