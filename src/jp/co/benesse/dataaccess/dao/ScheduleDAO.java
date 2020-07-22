dfa7b69e7315b937ba6173981fe74dcf9a2949b3
zaraki <anykomazaraki@gmail.com>
2020-07-23 01:51:24 +0900

    scheduleメソッド修正

--- src/jp/co/benesse/dataaccess/dao/ScheduleDAO.java
+++ src/jp/co/benesse/dataaccess/dao/ScheduleDAO.java
@@ -325,13 +325,17 @@ public class ScheduleDAO {
 	 */
 	public List<ScheduleBean> selectSchedule(int userId, String title) {
 		List<ScheduleBean> scheduleBeanList = new ArrayList<>();
+		if (title.contains("%")) {
+			title = title.replace("%", "\\%");
+		}
 
+		title = "%" + title + "%";
 		PreparedStatement preparedStatement = null;
 		try {
 
 			// SQLの定義
 			String sql = "SELECT * FROM SCHEDULE INNER JOIN PUBLIC.USER ON PUBLIC.USER.USER_ID = SCHEDULE.USER_ID "
-					+ "WHERE DELETE_FLAG = '0' AND TITLE LIKE '%?%' AND SCHEDULE.USER_ID = ?";
+					+ "WHERE DELETE_FLAG = '0' AND TITLE LIKE ? AND SCHEDULE.USER_ID = ?";
 			// SQLの作成(準備)
 			preparedStatement = this.connection.prepareStatement(sql);
 			preparedStatement.setString(1, title);
