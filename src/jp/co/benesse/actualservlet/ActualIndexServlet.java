f7136a1cdf160fce6d10dd0814f772d2f17774dc
zaraki <anykomazaraki@gmail.com>
2020-07-22 13:54:30 +0900

    修正

--- src/jp/co/benesse/actualservlet/ActualIndexServlet.java
+++ src/jp/co/benesse/actualservlet/ActualIndexServlet.java
@@ -2,6 +2,7 @@ package jp.co.benesse.actualservlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,14 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 public class ActualIndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request,response);
 	}
 
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/views/actual/actual_index.jsp");
+		dispatcher.forward(request, response);
+		return;
 	}
 
 }
