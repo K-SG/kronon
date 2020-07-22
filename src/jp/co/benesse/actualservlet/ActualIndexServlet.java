dd17074aada49ec6c7c3acb046004f08be5fb5ab
zaraki <anykomazaraki@gmail.com>
2020-07-22 17:14:10 +0900

    追記

--- src/jp/co/benesse/actualservlet/ActualIndexServlet.java
+++ src/jp/co/benesse/actualservlet/ActualIndexServlet.java
@@ -3,6 +3,8 @@ package jp.co.benesse.actualservlet;
 import java.io.IOException;
 import java.sql.Connection;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +14,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import jp.co.benesse.dataaccess.cm.ConnectionManager;
 import jp.co.benesse.dataaccess.dao.ScheduleDAO;
@@ -29,34 +30,58 @@ public class ActualIndexServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		//セッションスコープから値を取得
-		HttpSession session = request.getSession();
-		String userIdStr = (String) session.getAttribute("userId");
-		int userId = Integer.parseInt(userIdStr);
-		String userName = (String) session.getAttribute("userId");
+		// セッションスコープから値を取得
+//		HttpSession session = request.getSession();
+//		String userIdStr = (String) session.getAttribute("userId");
+//		int userId = Integer.parseInt(userIdStr);
+		int userId = 1;
+//		String userName = (String) session.getAttribute("userId");
+		String dateStr = request.getParameter("date");
 
+		LocalDate date;
 		List<ScheduleBean> scheduleBeanList = new ArrayList<>();
-		LocalDate scheduleDate = LocalDate.now();
+
 		// 遷移元の判定フラグ
 		String flag = "";
 
 		ConnectionManager connectionManager = new ConnectionManager();
-
 		ScheduleDAO scheduleDAO;
+
+		if (flag == null || dateStr == null) {// 実績確認画面以外から
+			date = LocalDate.now();
+		} else if (flag.equals("0")) {// 前月
+			date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+			LocalDate firstDayOfMonth = date.with(TemporalAdjusters.firstDayOfMonth()); // 初日
+			date = firstDayOfMonth.minusDays(1);// 先月の末日
+		} else if (flag.equals("1")) {// 翌月
+			date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+			LocalDate lastDayOfMonth = date.with(TemporalAdjusters.lastDayOfMonth()); // 末日
+			date = lastDayOfMonth.plusDays(1);// 次月の初日
+		} else {// 実績確認画面以外から
+			date = LocalDate.now();
+		}
 
 		try {
 			Connection connection = connectionManager.getConnection();
 			scheduleDAO = new ScheduleDAO(connection);
-			scheduleBeanList = scheduleDAO.getOneMonthSchedule(scheduleDate, userId);
+			scheduleBeanList = scheduleDAO.getOneMonthSchedule(date, userId);
+
+			// リクエストスコープにセット
+			request.setAttribute("date", date);
+			request.setAttribute("month", date.getMonthValue());
+			request.setAttribute("year", date.getYear());
+			request.setAttribute("scheduleBeanList", scheduleBeanList);
+
+			RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/views/actual/actual_index.jsp");
+			dispatcher.forward(request, response);
+			return;
 		} catch (RuntimeException e) {
-			connectionManager.rollback();
+			RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/views/error/error.jsp");
+			dispatcher.forward(request, response);
+			return;
 		} finally {
 			connectionManager.closeConnection();
 		}
-
-		RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/views/actual/actual_index.jsp");
-		dispatcher.forward(request, response);
-		return;
 	}
 
 }
