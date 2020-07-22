656960d3da6cfaacee13b1e736434b807edeaedf
zaraki <anykomazaraki@gmail.com>
2020-07-22 13:24:07 +0900

    必要なファイルを作成（中身はまだ）

---
+++ src/jp/co/benesse/actualservlet/ActualSearchServlet.java
@@ -0,0 +1,26 @@
+package jp.co.benesse.actualservlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/user/actualsearch")
+public class ActualSearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
