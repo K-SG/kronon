ce80d56d037e6ac502916247b913a9981772f902
shum-rmq <shunpei.kuri.fgt22@gmail.com>
2020-07-27 18:19:32 +0900

    省略以外できました

--- WebContent/js/scheduledetail.js
+++ WebContent/js/scheduledetail.js
@@ -2,11 +2,17 @@ $(function () {
 
 	let startTime = document.getElementById("startTime").textContent;
 	let endTime = document.getElementById("endTime").textContent;
+	let startTimee = document.getElementById("startTimee").textContent;
+	let endTimee = document.getElementById("endTimee").textContent;
 
 	const stTime = startTime.substring(0,5);
 	const edTime = endTime.substring(0,5);
 	$('#startTime').text(stTime) ;
 	$('#endTime').text(edTime) ;
+	const stTimee = startTimee.substring(0,5);
+	const edTimee = endTimee.substring(0,5);
+	$('#startTimee').text(stTimee) ;
+	$('#endTimee').text(edTimee) ;
 
 	/*DBと照合した後のエラーポップアップ表示*/
 	$(document).ready(function () {
@@ -23,8 +29,7 @@ $(function () {
 	  $('.close-popup').click(function () {
       $('.confirm-popup').fadeOut();
       $('.error-popup').fadeOut();
-      $('.back-popup').fadeOut();
-      $('.create-popup').fadeOut();
+
     });
 	  /*登録完了ポップアップのOKボタン押下時の遷移先*/
     $('.next-popup').click(function () {
