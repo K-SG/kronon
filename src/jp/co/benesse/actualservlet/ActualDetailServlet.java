a83914edb3d695a6fe35d90fa4b77a98114005c6
mmatsui-6454 <452012@tokyo.soka.ed.jp>
2020-07-27 17:01:37 +0900

    一次コミット

--- src/jp/co/benesse/actualservlet/ActualDetailServlet.java
+++ src/jp/co/benesse/actualservlet/ActualDetailServlet.java
@@ -1,12 +1,18 @@
 package jp.co.benesse.actualservlet;
 
 import java.io.IOException;
+import java.sql.Connection;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import jp.co.benesse.dataaccess.cm.ConnectionManager;
+import jp.co.benesse.dataaccess.dao.ScheduleDAO;
+import jp.co.benesse.dataaccess.value.ScheduleBean;
 
 /**
  * Servlet implementation class ActualDetailServlet
@@ -15,28 +21,42 @@ import javax.servlet.http.HttpServletResponse;
 public class ActualDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ActualDetailServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+//		int id = Integer.parseInt(request.getParameter("id"));
+		int id=1;
+
+		ConnectionManager connectionManager = new ConnectionManager();
+		ScheduleBean scheduleBean = new ScheduleBean();
+
+		try {
+
+			Connection connection = connectionManager.getConnection();
+			ScheduleDAO scheduleDAO = new ScheduleDAO(connection);
+
+			if (scheduleDAO.isDeleted(id)){
+				RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/views/error/error.jsp");
+				dispatcher.forward(request, response);
+				return;
+			}
+
+			scheduleBean = scheduleDAO.getScheduleByScheduleId(id);
+			System.out.println(scheduleBean);
+			request.setAttribute("scheduleBean", scheduleBean);
+
+			RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/views/actual/actual_detail.jsp");
+			dispatcher.forward(request, response);
+			return;
+
+		} catch (RuntimeException e) {
+			throw e;
+		} finally {
+			connectionManager.closeConnection();
+		}
+
 	}
 
 }
