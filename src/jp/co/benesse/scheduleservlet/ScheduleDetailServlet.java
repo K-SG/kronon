1329261102f7d3ca2fa0823bf1abe8c72fe0819a
mmatsui-6454 <452012@tokyo.soka.ed.jp>
2020-07-27 10:38:49 +0900

    scheduleBeanをリクエスト領域にセットしました

--- src/jp/co/benesse/scheduleservlet/ScheduleDetailServlet.java
+++ src/jp/co/benesse/scheduleservlet/ScheduleDetailServlet.java
@@ -2,7 +2,6 @@ package jp.co.benesse.scheduleservlet;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.text.SimpleDateFormat;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,9 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import jp.co.benesse.calc.Calc;
 import jp.co.benesse.dataaccess.cm.ConnectionManager;
 import jp.co.benesse.dataaccess.dao.ScheduleDAO;
 import jp.co.benesse.dataaccess.value.ScheduleBean;
@@ -46,32 +43,37 @@ public class ScheduleDetailServlet extends HttpServlet {
 			}
 
 			scheduleBean = scheduleDAO.getScheduleByScheduleId(id);
-			String actualTime = Calc.calcActualTime(scheduleBean);
-			SimpleDateFormat date = new SimpleDateFormat("yyyy/MM/dd");
-			String scheduleDate = date.format(scheduleBean.getScheduleDate());
-			SimpleDateFormat time = new SimpleDateFormat("HH:mm");
-			String scheduleStartTime = time.format(scheduleBean.getStartTime());
-			String scheduleEndTime = time.format(scheduleBean.getEndTime());
-
 			System.out.println(scheduleBean);
-			System.out.println(scheduleBean.getUserName());
-			System.out.println(actualTime);
-			System.out.println(scheduleDate);
-			System.out.println(scheduleStartTime);
-			System.out.println(scheduleEndTime);
-			System.out.println(scheduleBean.getPlace());
-			System.out.println(scheduleBean.getTitle());
-			System.out.println(scheduleBean.getContent());
+			request.setAttribute("scheduleBean", scheduleBean);
 
-			HttpSession session = request.getSession(true);
-			session.setAttribute("owner", scheduleBean.getUserName());
-			session.setAttribute("actualTime", actualTime);
-			session.setAttribute("schduleDate", scheduleDate);
-			session.setAttribute("startTime", scheduleStartTime);
-			session.setAttribute("endTime", scheduleEndTime);
-			session.setAttribute("place", scheduleBean.getPlace());
-			session.setAttribute("title", scheduleBean.getTitle());
-			session.setAttribute("content", scheduleBean.getContent());
+//			String actualTime = Calc.calcActualTime(scheduleBean);
+//			SimpleDateFormat date = new SimpleDateFormat("yyyy/MM/dd");
+//			String scheduleDate = date.format(scheduleBean.getScheduleDate());
+//			SimpleDateFormat time = new SimpleDateFormat("HH:mm");
+//			String scheduleStartTime = time.format(scheduleBean.getStartTime());
+//			String scheduleEndTime = time.format(scheduleBean.getEndTime());
+
+//			System.out.println(scheduleBean);
+//			System.out.println(scheduleBean.getUserName());
+//			System.out.println(actualTime);
+//			System.out.println(scheduleDate);
+//			System.out.println(scheduleStartTime);
+//			System.out.println(scheduleEndTime);
+//			System.out.println(scheduleBean.getPlace());
+//			System.out.println(scheduleBean.getTitle());
+//			System.out.println(scheduleBean.getContent());
+
+
+
+//			HttpSession session = request.getSession(true);
+//			session.setAttribute("owner", scheduleBean.getUserName());
+//			session.setAttribute("actualTime", actualTime);
+//			session.setAttribute("schduleDate", scheduleDate);
+//			session.setAttribute("startTime", scheduleStartTime);
+//			session.setAttribute("endTime", scheduleEndTime);
+//			session.setAttribute("place", scheduleBean.getPlace());
+//			session.setAttribute("title", scheduleBean.getTitle());
+//			session.setAttribute("content", scheduleBean.getContent());
 
 			RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/views/schedule/schedule_detail.jsp");
 			dispatcher.forward(request, response);
