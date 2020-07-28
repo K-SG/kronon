3ac0aa40eecc34209f4b855c2caf0567dce31967
mmatsui-6454 <452012@tokyo.soka.ed.jp>
2020-07-28 14:11:10 +0900

    修正完了

--- src/jp/co/benesse/actualservlet/ActualDetailServlet.java
+++ src/jp/co/benesse/actualservlet/ActualDetailServlet.java
@@ -25,8 +25,7 @@ public class ActualDetailServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// int scheduleId = Integer.parseInt(request.getParameter("scheduleId"));
-		int scheduleId = 2;
+		int scheduleId = Integer.parseInt(request.getParameter("scheduleId"));
 
 		ConnectionManager connectionManager = new ConnectionManager();
 		ScheduleBean scheduleBean = new ScheduleBean();
