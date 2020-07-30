883a9f75a823a1e1f9e63e3b20a55b6ca543f98e
yamamocchan <miyukiyamamoto564@gmail.com>
2020-07-30 11:43:36 +0900

    testメソッド追加

--- src/test/TestScheduleDAO.java
+++ src/test/TestScheduleDAO.java
@@ -13,68 +13,6 @@ import jp.co.benesse.dataaccess.dao.ScheduleDAO;
 import jp.co.benesse.dataaccess.value.ScheduleBean;
 
 public class TestScheduleDAO {
-/*
-	@org.junit.Test
-	public void testGetColor1() {
-		Color1 c1 = new Color1();
-		String t1 = c1.getColor(1);
-		assertThat(t1,is("赤"));
-	}
-
-	@org.junit.Test
-	public void testGetColor2() {
-		Color1 c1 = new Color1();
-		String t1 = c1.getColor(3);
-		assertThat(t1,is("1or2を入力して下さい"));
-	}
-
-	@org.junit.Test
-	public void testGetColor3() {
-		Color1 c1 = new Color1();
-		String t1 = c1.getColor(2);
-		assertThat(t1,is("青"));
-	}
-	//UserDAO
-	@org.junit.Test
-	public void userfindTest() {
-		ConnectionManager connectionManager = new ConnectionManager();
-		Connection connection = connectionManager.getConnection();
-		UserDAO userDAO = new UserDAO(connection);
-		UserBean bean=userDAO.findUser("test@email.com",  CryptographyLogic.encryptStr("9KY7fxp3e"));
-		assertThat(bean.getUserName(),is("test"));
-		assertThat(bean.getUserId(),is(2));
-	}
-	@org.junit.Test
-	public void userCreateTest(){
-		ConnectionManager connectionManager = new ConnectionManager();
-		Connection connection = connectionManager.getConnection();
-		UserDAO userDAO = new UserDAO(connection);
-		int answer = 0;
-		answer = userDAO.createUser("くろのす", "kronon@gmail.com", "Kronon11");
-		assertThat(answer, is(1));
-		System.out.println(answer);
-	}
-	@org.junit.Test
-	public void getUserNameTest(){
-		ConnectionManager connectionManager = new ConnectionManager();
-		Connection connection = connectionManager.getConnection();
-		UserDAO userDAO = new UserDAO(connection);
-		String answerStr = null;
-		answerStr = userDAO.getUserName(2);
-		assertThat(answerStr, is("test"));
-		System.out.println(answerStr);
-	}
-	@org.junit.Test
-	public void isBookingTest(){
-		ConnectionManager connectionManager = new ConnectionManager();
-		Connection connection = connectionManager.getConnection();
-		UserDAO userDAO = new UserDAO(connection);
-		Boolean answerFlg;
-		answerFlg = userDAO.isBooking("test@email.com");
-		assertThat(answerFlg, is(true));
-		System.out.println(answerFlg);
-	}
-*/
 
 /**********************************************************************/
 	/*ScheduleDAOのテスト*/
@@ -115,8 +53,8 @@ public class TestScheduleDAO {
 		Connection connection = connectionManager.getConnection();
 		ScheduleDAO scheduleDAO = new ScheduleDAO(connection);
 		List<ScheduleBean> answer;
-		answer = scheduleDAO.selectSchedule(1,Date.valueOf("2020-07-10"));
-		assertThat(answer.size(), is(5));//listが6件返される
+		answer = scheduleDAO.getOneMonthSchedule(Date.valueOf( "2020-07-10").toLocalDate(),1);
+		assertThat(answer.size(), is(7));//listが7件返される
 		System.out.println("listサイズ"+answer.size());
 	}
 	@org.junit.Test
@@ -125,8 +63,8 @@ public class TestScheduleDAO {
 		Connection connection = connectionManager.getConnection();
 		ScheduleDAO scheduleDAO = new ScheduleDAO(connection);
 		List<ScheduleBean> answer;
-		answer = scheduleDAO.selectSchedule(1,Date.valueOf("2020-07-27"),"ごはん");
-		assertThat(answer.size(), is(0));//Listが2件返される
+		answer = scheduleDAO.getOneMonthScheduleByTitle(1,Date.valueOf( "2020-07-27").toLocalDate(),"ごはん");
+		assertThat(answer.size(), is(2));//Listが2件返される
 		System.out.println(answer);
 	}
 
@@ -228,7 +166,7 @@ public class TestScheduleDAO {
 		bean.setScheduleDate(Date.valueOf("2020-07-10"));
 		bean.setStartTime(Time.valueOf("11:45:00"));
 		bean.setEndTime(Time.valueOf( "12:15:00"));
-		Boolean answerFlg;
+		boolean answerFlg;
 		answerFlg = scheduleDAO.isBooking(bean);
 		assertThat(answerFlg, is(true));
 		System.out.println(answerFlg);
@@ -243,9 +181,26 @@ public class TestScheduleDAO {
 		bean.setScheduleDate(Date.valueOf("2020-07-10"));
 		bean.setStartTime(Time.valueOf("10:45:00"));
 		bean.setEndTime(Time.valueOf( "11:45:00"));
-		Boolean answerFlg;
+		boolean answerFlg;
 		answerFlg = scheduleDAO.isBooking(bean);
 		assertThat(answerFlg, is(true));
+		System.out.println(answerFlg);
+	}
+
+	@org.junit.Test
+	public void isBookingTest7(){
+		ConnectionManager connectionManager = new ConnectionManager();
+		Connection connection = connectionManager.getConnection();
+		ScheduleDAO scheduleDAO = new ScheduleDAO(connection);
+		ScheduleBean bean=new ScheduleBean();
+		bean.setUserId(1);
+		bean.setScheduleId(8);
+		bean.setScheduleDate(Date.valueOf("2020-07-10"));
+		bean.setStartTime(Time.valueOf("08:30:00"));
+		bean.setEndTime(Time.valueOf( "08:45:00"));
+		boolean answerFlg;
+		answerFlg = scheduleDAO.isBooking(bean);
+		assertThat(answerFlg, is(false));
 		System.out.println(answerFlg);
 	}
 
@@ -254,7 +209,7 @@ public class TestScheduleDAO {
 		ConnectionManager connectionManager = new ConnectionManager();
 		Connection connection = connectionManager.getConnection();
 		ScheduleDAO scheduleDAO = new ScheduleDAO(connection);
-		Boolean answerFlg;
+		boolean answerFlg;
 		answerFlg = scheduleDAO.isDeleted(6);
 		assertThat(answerFlg, is(true));
 		System.out.println(answerFlg);
@@ -264,7 +219,7 @@ public class TestScheduleDAO {
 		ConnectionManager connectionManager = new ConnectionManager();
 		Connection connection = connectionManager.getConnection();
 		ScheduleDAO scheduleDAO = new ScheduleDAO(connection);
-		Boolean answerFlg;
+		boolean answerFlg;
 		answerFlg = scheduleDAO.isDeleted(3);
 		assertThat(answerFlg, is(false));
 		System.out.println(answerFlg);
@@ -274,10 +229,10 @@ public class TestScheduleDAO {
 		ConnectionManager connectionManager = new ConnectionManager();
 		Connection connection = connectionManager.getConnection();
 		ScheduleDAO scheduleDAO = new ScheduleDAO(connection);
-		Boolean answerFlg;
+		boolean answerFlg;
 		answerFlg = scheduleDAO.isDeleted(100);
 		assertThat(answerFlg, is(false));
-		System.out.println(answerFlg);
+		System.out.println("isdeleted3:"+answerFlg);
 	}
 	@org.junit.Test
 	public void tooLongSQLScheduleTest1(){
@@ -287,7 +242,7 @@ public class TestScheduleDAO {
 		List<ScheduleBean> answer;
 		answer = scheduleDAO.tooLongSQLSchedule(Date.valueOf("2020-07-07").toLocalDate(),1);
 		System.out.println("3件？"+answer);
-		assertThat(answer.size(), is(3));//2件が返ってくる→3件
+		assertThat(answer.size(), is(3));//3件
 	}
 	@org.junit.Test
 	public void tooLongSQLScheduleTest2(){
@@ -319,7 +274,6 @@ public class TestScheduleDAO {
 		answer = scheduleDAO.registerSchedule(bean);
 		assertThat(answer, is(1));
 		System.out.println(answer);
-		connectionManager.commit();
 	}
 /*	@org.junit.Test
 	public void registerScheduleTest2(){
