6fb8028bb1ab1bbdc7650c5be7ecbaef31553df6
limeJP <a_b_l_c@icloud.com>
2020-07-30 13:49:23 +0900

    治ってなかったので直しました

--- WebContent/js/schedule_edit.js
+++ WebContent/js/schedule_edit.js
@@ -82,7 +82,7 @@ $(function () {
     let lastDayOfMonth = new Date(year, month, 0).getDate();
 //    console.log(date,startHour,startMin,endHour,endMin,place,title,content);
 
-    if(date==""||startHour=='' || startMin=='' || endHour=='' || endMin=='' || place=='' || title==''){
+    if(startHour=='' || startMin=='' || endHour=='' || endMin=='' || place=='' || title==''){
     	popFlag='3';
     }else if(date==""){
     	//存在しない日付にした場合（2020/2/31）など
