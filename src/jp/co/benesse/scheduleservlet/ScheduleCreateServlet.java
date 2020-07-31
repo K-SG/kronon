d6639fabead7892477ee5d48b8fa9e747344cea2
limeJP <a_b_l_c@icloud.com>
2020-07-31 10:29:08 +0900

    20時まで行けるようにしました

--- src/jp/co/benesse/scheduleservlet/ScheduleCreateServlet.java
+++ src/jp/co/benesse/scheduleservlet/ScheduleCreateServlet.java
@@ -130,6 +130,7 @@ public class ScheduleCreateServlet extends HttpServlet {
 			connectionManager.closeConnection();
 		}
 
+
 	}
 
 }
--- src/jp/co/benesse/scheduleservlet/ScheduleUpdateServlet.java
+++ src/jp/co/benesse/scheduleservlet/ScheduleUpdateServlet.java
@@ -154,6 +154,7 @@ public class ScheduleUpdateServlet extends HttpServlet {
 //			connectionManager.closeConnection();
 		}
 
+
 	}
 
 }
