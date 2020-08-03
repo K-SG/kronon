861635ad2d0a413a09edfd3048df7c9aebd15366
nakane-benesse <nakane.benesse@gmail.com>
2020-08-03 13:42:07 +0900

    土曜日の祝日赤くしました。2021-09-23祝日にしました

--- WebContent/js/calendar_event.js
+++ WebContent/js/calendar_event.js
@@ -4,17 +4,25 @@ $(function () {
     const date_servlet = document.getElementById("date_servlet").value;
     const year = document.getElementById("year").textContent; //2020
     const month = document.getElementById("month").textContent ; ; //7
+    let holiday_flag = 0;//祝日だけホバーがきかないのでflagで対応
 
 	$('td').hover(
-
 		function(){
 			if($(this).hasClass('disabled')){
 				return;
+			}
+			if($(this).hasClass('holiday')){
+				holiday_flag = 1;
+				$(this).removeClass('holiday');
 			}
 			$(this).addClass('cell-active');
 		},
 		function(){
 			$(this).removeClass('cell-active');
+			if(holiday_flag===1){
+				$(this).addClass('holiday');
+				holiday_flag=0;
+			}
 		}
 	)
 
