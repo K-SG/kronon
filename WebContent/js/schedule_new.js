1055f1186ac57190df3da67f33e2b1f52bc5a2f3
SugiKoki <fnvfm00957@yahoo.co.jp>
2020-08-03 11:59:35 +0900

    scheduleeditまでインデント修正しました

--- WebContent/js/schedule_new.js
+++ WebContent/js/schedule_new.js
@@ -1,195 +1,185 @@
-$(function () {
+$(function() {
 
-/*日付に今日の日付が挿入されるようにする*/
-  window.onload = function () {
-	  /*予定がかぶっていた時などは日付が保持されるようにするための処理*/
-	  if(document.getElementById("date").value == ""){
-	    let date = new Date();
-	    date.setDate(date.getDate());
-	    let yyyy = date.getFullYear();
-	    let mm = ("0"+(date.getMonth()+1)).slice(-2);
-	    let dd = ("0"+date.getDate()).slice(-2);
-	    document.getElementById("date").value=yyyy+'-'+mm+'-'+dd;
-	  }
+	/* 日付に今日の日付が挿入されるようにする */
+	window.onload = function() {
+		/* 予定がかぶっていた時などは日付が保持されるようにするための処理 */
+		if (document.getElementById("date").value == "") {
+			let date = new Date();
+			date.setDate(date.getDate());
+			let yyyy = date.getFullYear();
+			let mm = ("0" + (date.getMonth() + 1)).slice(-2);
+			let dd = ("0" + date.getDate()).slice(-2);
+			document.getElementById("date").value = yyyy + '-' + mm + '-' + dd;
+		}
 
-
-		/*DBと照合した後のポップアップフラグ*/
+		/* DBと照合した後のポップアップフラグ */
 		var popFlag = document.getElementById('flag').value;
 
-		/*ScheduleCreateServlet返ってきたとき、値の保存をする*/
-		if(popFlag==='0'||popFlag==='1'){
+		/* ScheduleCreateServlet返ってきたとき、値の保存をする */
+		if (popFlag === '0' || popFlag === '1') {
 			let date = document.getElementById('set-date').value;
 			let startTime = document.getElementById('set-start-time').value;
 			let endTime = document.getElementById('set-end-time').value;
 			let startHour = startTime.substring(0, 2);
-			let startMin = startTime.substring(3,5);
+			let startMin = startTime.substring(3, 5);
 			let endHour = endTime.substring(0, 2);
-			let endMin = endTime.substring(3,5);
+			let endMin = endTime.substring(3, 5);
 			let place = document.getElementById('set-place').value;
 
-			console.log(startHour,startMin,endMin);
+			console.log(startHour, startMin, endMin);
 
-			/*すべての初期選択を外す*/
+			/* すべての初期選択を外す */
 			$('select option').attr('selected', false);
-			/*開始時間と終了時間を09→9に変換*/
+			/* 開始時間と終了時間を09→9に変換 */
 			if (startHour.slice(0, 1) == 0) {
 				startHour = startHour.substring(1, 2);
 			}
 			if (endHour.slice(0, 1) == 0) {
 				endHour = endHour.substring(1, 2);
 			}
-			/*初期選択がされるようにselectedをつける*/
-				$('#starthour').val(startHour);
-				$('#startminutes').val(startMin);
-				$('#endhour').val(endHour);
-				$('#endminutes').val(endMin);
-				$('#new-place').val(place);
+			/* 初期選択がされるようにselectedをつける */
+			$('#starthour').val(startHour);
+			$('#startminutes').val(startMin);
+			$('#endhour').val(endHour);
+			$('#endminutes').val(endMin);
+			$('#new-place').val(place);
 		}
 
-
-		/*登録が完了した場合*/
-		  if(popFlag === '0'){
-		  	$('.create-msg').html('登録が完了したよ！');
-		  	$('.complete-popup').fadeIn();
-		  	return;
-		  }
-		  if(popFlag === '1'){
-			  	$('.new-msg').html('予定がかぶってるよ！');
-			  	$('.error-popup').fadeIn();
-			  	return;
-		  }
+		/* 登録が完了した場合 */
+		if (popFlag === '0') {
+			$('.create-msg').html('登録が完了したよ！');
+			$('.complete-popup').fadeIn();
+			return;
+		}
+		if (popFlag === '1') {
+			$('.new-msg').html('予定がかぶってるよ！');
+			$('.error-popup').fadeIn();
+			return;
+		}
 	}
 
-  /*登録ボタンを押した際のエラーチェックとポップアップ表示*/
-  $('#ok-button').click(function () {
-	    let date = document.getElementById('date').value;
-	    let startHour = document.getElementById('starthour').value;
-	    let startMin = document.getElementById('startminutes').value;
-	    let endHour = document.getElementById('endhour').value;
-	    let endMin = document.getElementById('endminutes').value;
-	    let place = document.getElementById('new-place').value;
-	    let title=document.getElementById('title').value;
-	    let content=document.getElementById('content').value;
-	    title=title.trim();
+	/* 登録ボタンを押した際のエラーチェックとポップアップ表示 */
+	$('#ok-button').click(
+		function() {
+			let date = document.getElementById('date').value;
+			let startHour = document.getElementById('starthour').value;
+			let startMin = document.getElementById('startminutes').value;
+			let endHour = document.getElementById('endhour').value;
+			let endMin = document.getElementById('endminutes').value;
+			let place = document.getElementById('new-place').value;
+			let title = document.getElementById('title').value;
+			let content = document.getElementById('content').value;
+			title = title.trim();
 
-    let d = new Date(date);
-    let weekday = '日月火水木金土'[d.getDay()];
-    //リリース年と月の取得
-    let releaseYear = 2020;
-    let releaseMonth = 8;
-    //サービス終了年と月の取得
-    let releaseLastYear = 2023;
-    let releaseLastMonth = 8;
-  //年、月、日をそれぞれ取得
-    let year = date.substring(0, 4);
-    let month = date.substring(5, 7);
-    let day = date.substring(8, 10);
-  //月と日付を07→7に変換
-    if (month.slice(0, 1) == 0) {
-        month = month.substring(1, 2);
-    }
-    if (day.slice(0, 1) == 0) {
-        day = day.substring(1, 2);
-    }
-  //月初、月末の日付を取得
-    let firstDayOfMonth = new Date(year, month - 1, 1).getDate();
-    let lastDayOfMonth = new Date(year, month, 0).getDate();
+			let d = new Date(date);
+			let weekday = '日月火水木金土'[d.getDay()];
+			// リリース年と月の取得
+			let releaseYear = 2020;
+			let releaseMonth = 8;
+			// サービス終了年と月の取得
+			let releaseLastYear = 2023;
+			let releaseLastMonth = 8;
+			// 年、月、日をそれぞれ取得
+			let year = date.substring(0, 4);
+			let month = date.substring(5, 7);
+			let day = date.substring(8, 10);
+			// 月と日付を07→7に変換
+			if (month.slice(0, 1) == 0) {
+				month = month.substring(1, 2);
+			}
+			if (day.slice(0, 1) == 0) {
+				day = day.substring(1, 2);
+			}
+			// 月初、月末の日付を取得
+			let firstDayOfMonth = new Date(year, month - 1, 1).getDate();
+			let lastDayOfMonth = new Date(year, month, 0).getDate();
+			// ポッププラグ変数作成
+			var popFlag;
+			if (startHour == '' || startMin == '' || endHour == '' || endMin == '' || place == '' || title == '') {
+				popFlag = '3';
+			} else if (date == "") {
+			// 存在しない日付にした場合（2020/2/31）など
+				popFlag = '4';
+			} else if ((startHour * 60 + startMin) - (endHour * 60 + endMin) >= 0) {
+			// 終了時間よりも開始時間のほうが遅かったら
+				popFlag = '4';
+			} else if (!(firstDayOfMonth <= day && day <= lastDayOfMonth)) {
+			// 存在しない日付を入力したら（2/31など）
+				popFlag = '4';
+			} else if (year < releaseYear || (year == releaseYear && month < releaseMonth)) {
+			// リリース前の日付を選択したら
+				popFlag = '4';
+			} else if (year > releaseLastYear || (year == releaseLastYear && month > releaseLastMonth)) {
+			// サービス終了後の日付を選択したら
+				popFlag = '4';
+			} else {
+				popFlag = '5';
+			}
 
-    //ポッププラグ変数作成
-    var popFlag;
+			if (popFlag === '3') {
+				$('.new-msg').html('入力されていない<br>項目があるよ！');
+				$('.error-popup').fadeIn();
+				return;
+			} else if (popFlag === '4') {
+				$('.new-msg').html('日付や時間の入力が<br>おかしいよ！');
+				$('.error-popup').fadeIn();
+				return;
+			} else if (popFlag === '5') {
+				$('#time-msg').html(year + '/' + month + '/' + day + '('+ weekday + ')' + startHour + ':'+ startMin + '～' + endHour + ':'+ endMin);
+				$('#title-msg').html(title);
+				$('#content-msg').html(content);
+				$('#confirm-popup2').fadeIn();
+				return;
+			}
+	});
 
-//    console.log(date,startHour,startMin,endHour,endMin,place,title,content);
+	/* 確認ポップアップのOKを押した際の動き */
+	$('#confirm-ok').click(function() {
+		$('.schedule-new-form').submit();
+		return;
+	});
 
-    if(startHour=='' || startMin=='' || endHour=='' || endMin=='' || place=='' || title==''){
-    	popFlag='3';
+	/* 登録完了ポップアップのOKボタン押下時の遷移先 */
+	$('.next-popup').click(function() {
+		let date = document.getElementById('date').value;
+		location.href = "calendar?date=" + date;
+	});
 
-    }else if(date==""){
-    	//存在しない日付にした場合（2020/2/31）など
-    	popFlag='4';
-    }else if((startHour*60+startMin)-(endHour*60+endMin)>=0){
-    	//終了時間よりも開始時間のほうが遅かったら
-//    	console.log("開始時間のほうが遅い");
-    	popFlag='4';
-    }else if(!(firstDayOfMonth <= day && day <= lastDayOfMonth)){
-    	//存在しない日付を入力したら（2/31など）
-//    	console.log("存在しない日付");
-    	popFlag='4';
-    }else if(year < releaseYear || (year == releaseYear && month < releaseMonth)){
-    	//リリース前の日付を選択したら
-//    	console.log("リリース前の日付");
-    	popFlag='4';
-    }else if(year > releaseLastYear || (year == releaseLastYear && month > releaseLastMonth)){
-    	//サービス終了後の日付を選択したら
-//    	console.log("サービス終了後の日付");
-    	popFlag='4';
-    }else{
-    	popFlag='5';
-    }
+	/* キャンセルボタンを押した際のポップアップ表示 */
+	$('#cancel-button').click(function() {
+		$('.cancel-popup').fadeIn();
+		return;
+	});
 
-    if(popFlag==='3'){
-        $('.new-msg').html('入力されていない<br>項目があるよ！');
-        $('.error-popup').fadeIn();
-        return;
-    }else if(popFlag==='4'){
-        $('.new-msg').html('日付や時間の入力が<br>おかしいよ！');
-        $('.error-popup').fadeIn();
-        return;
-    }else if(popFlag==='5'){
-        $('#time-msg').html(year + '/' + month + '/' + day +'(' + weekday + ')' + startHour +':'+ startMin +'～'+ endHour +':'+ endMin);
-        $('#title-msg').html(title);
-        $('#content-msg').html(content);
-    	$('#confirm-popup2').fadeIn();
-        return;
-    }
-  });
-
-  /*確認ポップアップのOKを押した際の動き*/
-  $('#confirm-ok').click(function () {
-      $('.schedule-new-form').submit();
-      return;
+	/* ポップアップを閉じる際の動き */
+	$('.close-popup').click(function() {
+		$('#confirm-popup2').fadeOut();
+		$('.error-popup').fadeOut();
+		$('.cancel-popup').fadeOut();
+		$('.complete-popup').fadeOut();
+	});
 });
 
-  /*登録完了ポップアップのOKボタン押下時の遷移先*/
-  $('.next-popup').click(function () {
-  let date = document.getElementById('date').value;
-    location.href= "calendar?date=" + date;
-  });
-
-    /*キャンセルボタンを押した際のポップアップ表示*/
-    $('#cancel-button').click(function () {
-        $('.cancel-popup').fadeIn();
-        return;
-  });
-
-  /*ポップアップを閉じる際の動き*/
-  $('.close-popup').click(function () {
-	$('#confirm-popup2').fadeOut();
-    $('.error-popup').fadeOut();
-    $('.cancel-popup').fadeOut();
-    $('.complete-popup').fadeOut();
-  });
-
-});
-//20時選択したとき分のプルダウンが0に
+// 20時選択したとき分のプルダウンが0に
 function selectboxChange1() {
-    let selindex = document.form.starthour.selectedIndex;
-    switch (selindex) {
-      case 12:
-      	document.form.startminutes.selectedIndex=0;
-      	document.form.startminutes.disabled=true;
-        break;
-		default:
-			document.form.startminutes.disabled=false;
-    }
-  }
-  function selectboxChange2() {
-	        let selindex = document.form.endhour.selectedIndex;
-	        switch (selindex) {
-	          case 12:
-	          	document.form.endminutes.selectedIndex=0;
-	          	document.form.endminutes.disabled=true;
-	            break;
-	  		default:
-	  			document.form.endminutes.disabled=false;
-	        }
+	let selindex = document.form.starthour.selectedIndex;
+	switch (selindex) {
+	case 12:
+		document.form.startminutes.selectedIndex = 0;
+		document.form.startminutes.disabled = true;
+		break;
+	default:
+		document.form.startminutes.disabled = false;
 	}
+}
+function selectboxChange2() {
+	let selindex = document.form.endhour.selectedIndex;
+	switch (selindex) {
+	case 12:
+		document.form.endminutes.selectedIndex = 0;
+		document.form.endminutes.disabled = true;
+		break;
+	default:
+		document.form.endminutes.disabled = false;
+	}
+}
--- WebContent/js/user_new.js
+++ WebContent/js/user_new.js
@@ -5,43 +5,41 @@ $(function () {
 	/*DBと照合した後のエラーポップアップ表示*/
 	$(document).ready(function () {
 		let popFlag = document.getElementById('flag').value;
-
-		/*登録が完了した場合*/
+		  /*登録が完了した場合*/
 		  if(popFlag == 0){
-		  	$('.create-msg').html('登録が完了したよ！');
-		  	$('.complete-popup').fadeIn();
-		  	return;
+			  $('.create-msg').html('登録が完了したよ！');
+			  $('.complete-popup').fadeIn();
+			  return;
 		  }
-		/*メールアドレスがすでに登録されていた場合*/
+		  /*メールアドレスがすでに登録されていた場合*/
 		  if(popFlag == 1){
-		  	$('.create-popup').fadeIn();
-		  	return;
+			  $('.create-popup').fadeIn();
+			  return;
 		  }
 	});
 
 	/*新規登録ボタンを押下した際のエラーチェックとポップアップ表示*/
-	  $('.user-create-button').click(function () {
+	$('.user-create-button').click(function () {
 		let popFlag = document.getElementById('flag').value;
 		let userName = document.getElementById('create_name').value;
 	    let mail = document.getElementById('create_mail').value;
 	    let password1 = document.getElementById('create_password1').value;
 	    let password2 = document.getElementById('create_password2').value;
 
-	    if(userName.trim()==''|| mail=='' || password1=='' || password2==''){
-	    	popFlag=2;
+	    if(userName.trim() == '' || mail == '' || password1 == '' || password2 == ''){
+	    	popFlag = 2;
 	    }else if(!mail.match(/^[A-Za-z0-9]+[\w\-_]+@[\w\._]+\.\w{2,}$/)){
-	    	popFlag=3;
+	    	popFlag = 3;
         }else if(password1.length < 8 || !(password1.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)&& password1.match(/([0-9])/)) || !(password1.match(/([a-zA-Z])/) && password1.match(/([0-9])/))){
-	        popFlag=4;
-	    }else if(password1!==password2){
-	        popFlag=5;
+	        popFlag = 4;
+	    }else if(password1 !== password2){
+	        popFlag = 5;
 	    }else{
-	      popFlag = 0;
+	    	popFlag = 0;
 	    }
-
 	    if(popFlag == 0){
 	        /*表示名の値をセット*/
-	        let nameText = document.getElementById("confirmUserName");
+	    	let nameText = document.getElementById("confirmUserName");
 			nameText.innerText = document.forms.id_create_form.create_name.value;
 			/*メールの値をセット*/
 			let mailText = document.getElementById("confirmMail");
@@ -55,46 +53,45 @@ $(function () {
 				str += '＊';
 			}
 			passwordText.innerText = str;
-
 	        $('.confirm-popup').fadeIn();
 	        $('user-create-form').submit();
 	        return;
 	      }
-	    if(popFlag==2){
+	    if(popFlag == 2){
 	        $('.create-msg').html('入力されていない<br>項目があるよ！');
 	        $('.create-popup').fadeIn();
 	        return;
 	      }
-	    if(popFlag==3){
+	    if(popFlag == 3){
 	        $('.create-msg').html('メールアドレスが<br>不正だよ！');
 	        $('.create-popup').fadeIn();
 	        return;
 	      }
-	    if(popFlag==4){
+	    if(popFlag == 4){
 	        $('.create-msg').html('パスワードの条件を<br>満たしていないよ！');
 	        $('.create-popup').fadeIn();
 	        return;
 	      }
-	    if(popFlag==5){
+	    if(popFlag == 5){
 	        $('.create-msg').html('パスワードが<br>一致していないよ！');
 	        $('.create-popup').fadeIn();
 	        return;
 	      }
 	  });
-	  /*ポップアップを閉じる*/
-	  $('.close-popup').click(function () {
-        $('.confirm-popup').fadeOut();
+
+	/*ポップアップを閉じる*/
+	$('.close-popup').click(function () {
+		$('.confirm-popup').fadeOut();
         $('.error-popup').fadeOut();
         $('.back-popup').fadeOut();
         $('.create-popup').fadeOut();
-      });
-	  /*登録完了ポップアップのOKボタン押下時の遷移先*/
-      $('.next-popup').click(function () {
+	});
+	/*登録完了ポップアップのOKボタン押下時の遷移先*/
+	$('.next-popup').click(function () {
         location.href= "/kronon/user/calendar";
-      });
-
-      $('#submit-user').click(function(){
-    	   $('#id_create_form').submit();
-    	   return;
-      })
+	});
+	$('#submit-user').click(function(){
+		$('#id_create_form').submit();
+		return;
+	})
 });
